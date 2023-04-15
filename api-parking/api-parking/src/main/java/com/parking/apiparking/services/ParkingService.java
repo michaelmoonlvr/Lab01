69bb7235727d9efdaea29a1a1cc4811487b3133b
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2023-04-15 09:24:06 -0500

    feat: Crear la clase parkingService. Implemento los metodos getAllCars y addCar

--- api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
@@ -1,4 +1,26 @@
 package com.parking.apiparking.services;
 
+import com.parking.apiparking.entities.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingService {
+
+    private List<Car> parkingLot;
+
+    public ParkingService(){
+        this.parkingLot = new ArrayList<>();
+    }
+
+    //TODO:US 01: Como usuario, quiero poder ver la lista de todos los autos estacionados en el
+    //parqueo, para poder tener una visión general del estado actual del estacionamiento.
+    public List<Car> getAllCars(){
+        return this.parkingLot;
+    }
+
+    //TODO: US 02 Como usuario, quiero poder agregar un auto al parqueo, para poder estacionar mi vehículo
+    public void addCar(Car car){
+        this.parkingLot.add(car);
+    }
 }
